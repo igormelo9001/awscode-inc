cb2e0dabac09442e9e322911cadd2d439ca430c1
unknown <igormelo9001@gmail.com>
2020-04-14 11:14:49 -0300

    refactor the name of application class

--- src/main/java/com/awscode/inc/employees/Application.java
+++ src/main/java/com/awscode/inc/employees/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EmployeesApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EmployeesApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
