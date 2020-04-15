9dcb655e8e5e9275b8d36a459c2da3d3ad3b3247
unknown <igormelo9001@gmail.com>
2020-04-15 12:18:53 -0300

    updated controller with more features

--- src/main/java/com/awscode/inc/employees/controller/EmployeeController.java
+++ src/main/java/com/awscode/inc/employees/controller/EmployeeController.java
@@ -1,8 +1,72 @@
 package com.awscode.inc.employees.controller;
 
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.hibernate.mapping.Map;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.awscode.inc.employees.model.Employee;
+import com.awscode.inc.employees.repository.EmployeeRepository;
+
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
+@RequestMapping("/api/v1")
 public class EmployeeController {
 
-}
+	@Autowired
+	private EmployeeRepository employeeRepository;
+	
+	@GetMapping("/employees")
+	public List<Employee> getAllEmployees(){
+		return employeeRepository.findAll();
+	}
+	
+	@GetMapping("/employees/{id}")
+	public ResponseEntity<Employee> getEmployeeById(@PathVariable(value = "id") Long employeeId)
+	throws Exception{
+		Employee employee = employeeRepository.findById(employeeId)
+				.orElseThrow(() -> new Exception("Employee not found for this id " + employeeId));
+		return ResponseEntity.ok().body(employee);
+	}
+	
+	@PostMapping("/employees")
+	public Employee createEmployee(@Valid @RequestBody Employee employee) {
+		return employeeRepository.save(employee);
+	}
+	
+	@PutMapping("/employees/{id}")
+	public ResponseEntity<Employee> updateEmployee(@PathVariable(value = "id") Long employeeId,
+			@Valid @RequestBody Employee employeeDetails) throws Exception {
+		Employee employee = employeeRepository.findById(employeeId)
+				.orElseThrow(() -> new Exception("Employee not found for this id " + employeeId));
+		
+		employee.setName(employeeDetails.getName());
+		employee.setBornDate(employeeDetails.getBornDate());
+		employee.setSalary(employeeDetails.getSalary());
+		employee.setPosition(employeeDetails.getPosition());
+		final Employee updatedEmployee = employeeRepository.save(employee);
+		return ResponseEntity.ok(updatedEmployee);
+	}
+	
+	@DeleteMapping("/employees/{id}")
+	public ResponseEntity<?> deleteEmployee(@PathVariable Long id) {
+		return employeeRepository.findById(id)
+				.map(record -> {
+					employeeRepository.deleteById(id);
+					return ResponseEntity.ok().build();
+				}).orElse(ResponseEntity.notFound().build());
+	}
+	
+ }
